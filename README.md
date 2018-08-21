f4b71c3ff966131cc401819f3d4d5a0426be4718
rxdey <jyh1994@qq.com>
2018-08-21 10:39:07 +0800

    addinfo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# myProject
-这是一个测试项目。
+# bangumi
+emm 试着写了简单的爬虫爬取bangumi动漫目录然后重新整理下
--- server/app/router/index.js
+++ server/app/router/index.js
@@ -27,7 +27,35 @@ router.get('/api/getList', async (ctx, next) => {
     type: type
   })
   ctx.body = list
+})
 
+router.all('/api/getRank',async (ctx,next) => {
+  let ranks = [
+    { id:1, name:'洞洞1', score:1508},
+    { id:2, name:'洞洞2', score:158},
+    { id:3, name:'洞洞3', score:152},
+    { id:4, name:'洞洞4', score:158},
+    { id:5, name:'洞洞5', score:122},
+    { id:6, name:'洞洞6', score:155},
+    { id:7, name:'洞洞7', score:155},
+    { id:8, name:'洞洞8', score:155},
+    { id:9, name:'洞洞9', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+    { id:10, name:'洞洞10', score:155},
+  ]
+  ctx.body = {
+    data:ranks
+  }
 })
 
 
